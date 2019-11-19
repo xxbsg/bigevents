3636df52fc35bab97fd065facdf22451b1ded6e8
xxg <qq1102746402@163.com>
2019-11-19 22:03:51 +0800

    ip地址属性化存储

--- web_back/js/untils/user.js
+++ web_back/js/untils/user.js
@@ -1,27 +1,27 @@
 var user = {
-
+    baseurl:'http://localhost:8000',
     login: function (name, pwd, callback) {
 
-        $.post('http://localhost:8000/admin/login', { user_name: name, password: pwd }, function (res) {
+        $.post(this.baseurl+'/admin/login', { user_name: name, password: pwd }, function (res) {
             callback(res)
 
         })
 
     },
     logout: function (callback) {
-        $.post('http://localhost:8000/admin/logout', function (res) {
+        $.post(this.baseurl+'/admin/logout', function (res) {
             callback(res)
 
         })
     },
     getInfo: function (callback) {
-        $.get('http://localhost:8000/admin/getuser', function (res) {
+        $.get(this.baseurl+'/admin/getuser', function (res) {
             callback(res)
 
         })
     },
     getcontentcount:function(callback){
-        $.get('http://localhost:8000/admin/article_count',function(res){
+        $.get(this.baseurl+'/admin/article_count',function(res){
             // console.log(res);
             callback(res);
 
