c8f6ee061bfa5fa7bd6872d740415bb77af1b803
xxg <qq1102746402@163.com>
2019-11-19 21:40:45 +0800

    实现用户信息显示  文章统计

--- web_back/js/untils/user.js
+++ web_back/js/untils/user.js
@@ -1,18 +1,24 @@
-var user={
+var user = {
 
-    login:function(name,pwd,callback){
+    login: function (name, pwd, callback) {
 
         $.post('http://localhost:8000/admin/login', { user_name: name, password: pwd }, function (res) {
-           callback(res)
+            callback(res)
 
         })
 
     },
-    logout:function(callback){
-        $.post('http://localhost:8000/admin/logout',function(res){
-                callback(res)
-                
-            })
+    logout: function (callback) {
+        $.post('http://localhost:8000/admin/logout', function (res) {
+            callback(res)
+
+        })
+    },
+    getInfo: function (callback) {
+        $.get('http://localhost:8000/admin/getuser', function (res) {
+            callback(res)
+
+        })
     }
 
 
