0088ee2934ac2251ded64145e5cb3c51e2cc889d
xxg <qq1102746402@163.com>
2019-11-21 10:11:36 +0800

    实现评论 且封装完成

--- web_back/js/untils/user.js
+++ web_back/js/untils/user.js
@@ -1,33 +1,45 @@
 var user = {
-    baseurl:'http://localhost:8000',
+    baseurl: 'http://localhost:8000',
+    //登录
     login: function (name, pwd, callback) {
 
-        $.post(this.baseurl+'/admin/login', { user_name: name, password: pwd }, function (res) {
+        $.post(this.baseurl + '/admin/login', { user_name: name, password: pwd }, function (res) {
             callback(res)
 
         })
 
     },
+    //退出
     logout: function (callback) {
-        $.post(this.baseurl+'/admin/logout', function (res) {
+        $.post(this.baseurl + '/admin/logout', function (res) {
             callback(res)
 
         })
     },
+    //获取用户信息
     getInfo: function (callback) {
-        $.get(this.baseurl+'/admin/getuser', function (res) {
+        $.get(this.baseurl + '/admin/getuser', function (res) {
             callback(res)
 
         })
     },
-    getcontentcount:function(callback){
-        $.get(this.baseurl+'/admin/article_count',function(res){
+    //文章相关
+    //文章数量统计
+    getcontentcount: function (callback) {
+        $.get(this.baseurl + '/admin/article_count', function (res) {
             // console.log(res);
             callback(res);
 
-            
+
+        })
+    },
+    //文章评论统计
+    getremarkcount: function (callback) {
+        $.get(this.baseurl + '/admin/comment_count', function (res) {
+           callback(res)
         })
     }
+
 
 
 
