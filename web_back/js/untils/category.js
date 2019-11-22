1d3749e38edf31c93906d47da56f5cff83c39a07
xxg <qq1102746402@163.com>
2019-11-22 09:58:15 +0800

    实现文章分类的增删改查

--- web_back/js/untils/category.js
+++ web_back/js/untils/category.js
@@ -1,3 +1,22 @@
 var category={
-    
+    getcategory:function(callback){
+        $.get(APIURL.getcategory,function(res){
+            callback(res)
+        })
+    },
+    addcategory:function(name,slug,callback){
+        $.post(APIURL.addcategory,{'name':name,'slug':slug},function(res){
+            callback(res)
+        })
+    },
+    delcategory:function(id,callback){
+        $.post(APIURL.delcategory,{'id':id},function(res){
+            callback(res)
+        })
+    },
+    editcategory:function(id,name,slug,callback){
+        $.post(APIURL.editcategory,{'id':id,'name':name,'slug':slug},function(res){
+            callback(res)
+        })
+    }
 }
--- web_back/js/untils/config.js
+++ web_back/js/untils/config.js
@@ -7,5 +7,13 @@ var APIURL={
     getcontentcount:baseurl + '/admin/article_count',
     getremarkcount:baseurl + '/admin/comment_count',
     getmonthcontentcount:baseurl + '/admin/month_article_count',
+
+    //分类
+    getcategory:baseurl +'/admin/category_search',//获取分类
+    addcategory:baseurl +'/admin/category_add',//新增分类
+    delcategory:baseurl +'/admin/category_delete',//删除分类
+    editcategory:baseurl +'/admin/category_edit',//修改分类
     
+
+
 }
